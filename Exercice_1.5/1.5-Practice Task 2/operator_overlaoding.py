3a3afcddf33f6638e8891e05648bd3b0b2b06507
xavsln <19362582+xavsln@users.noreply.github.com>
2022-11-28 17:18:02 +0100

    Add Practice Task 2

---
+++ Exercice_1.5/1.5-Practice Task 2/operator_overlaoding.py
@@ -0,0 +1,55 @@
+class Height(object):
+  def __init__(self, feet, inches):
+    self.feet = feet
+    self.inches = inches
+
+  def __str__(self):
+    output = str(self.feet) + " feet, " + str(self.inches) + " inches"
+    return output
+
+  def __add__(self, other):
+    # Converting both objects' heights into inches
+    height_A_inches = self.feet * 12 + self.inches
+    height_B_inches = other.feet * 12 + other.inches
+
+    # Adding them up
+    total_height_inches = height_A_inches + height_B_inches
+
+    # Getting the output in feet
+    output_feet = total_height_inches // 12
+
+    # Getting the output in inches
+    output_inches = total_height_inches - (output_feet * 12)
+
+    # Returning the final output as a new Height object
+    return Height(output_feet, output_inches)
+
+  def __sub__(self, other):
+    # Converting both objects' heights into inches
+    height_C_inches = self.feet * 12 + self.inches
+    height_D_inches = other.feet * 12 + other.inches
+
+    # Substracting them up
+    total_height_inches = height_D_inches + height_C_inches
+
+    # Getting the output in feet
+    output_feet = total_height_inches // 12
+
+    # Getting the output in inches
+    output_inches = total_height_inches - (output_feet * 12)
+
+    # Returning the final output as a new Height object
+    return Height(output_feet, output_inches)
+
+
+person_A_height = Height(5, 10)
+person_B_height = Height(4, 10)
+height_sum = person_A_height + person_B_height
+
+print("Total height (after height sum):", height_sum)
+
+person_C_height = Height(3, 9)
+person_D_height = Height(5, 10)
+height_sub = person_D_height - person_C_height
+
+print("Total height (after height sub):", height_sub)
