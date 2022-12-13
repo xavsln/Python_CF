75739f8f93a77c0c44193d528ce304ff6e0b05cb
xavsln <19362582+xavsln@users.noreply.github.com>
2022-12-13 17:31:19 +0100

    Implement data analysis and visualization functionalities

--- Exercise_2.3/Bookstore project/src/sales/views.py
+++ Exercise_2.3/Bookstore project/src/sales/views.py
@@ -3,6 +3,16 @@ from django.shortcuts import render
 #to protect function-based views
 from django.contrib.auth.decorators import login_required
 
+from .forms import SalesSearchForm
+
+from .models import Sale
+
+import pandas as pd
+
+from .utils import get_bookname_from_id, get_chart
+
+
+
 # Create your views here.
 def home(request):
   return render(request, 'sales/home.html')
@@ -10,8 +20,41 @@ def home(request):
 #define function-based view - records(records()
 #keep protected
 @login_required
+#define function-based view - records()
 def records(request):
-  #do nothing, simply display page    
-  return render(request, 'sales/records.html')
+  #create an instance of SalesSearchForm that you defined in sales/forms.py
+  form = SalesSearchForm(request.POST or None)
+  sales_df=None   #initialize dataframe to None
+  chart = None    #initialize chart to None
+  #check if the button is clicked
+  if request.method =='POST':
+    #read book_title and chart_type
+    book_title = request.POST.get('book_title')
+    chart_type = request.POST.get('chart_type')
+
+    #apply filter to extract data
+    qs =Sale.objects.filter(book__name=book_title)
+    if qs:      #if data found
+      #convert the queryset values to pandas dataframe
+      sales_df=pd.DataFrame(qs.values()) 
+      #convert the ID to Name of book
+      sales_df['book_id']=sales_df['book_id'].apply(get_bookname_from_id)
+
+      #call get_chart by passing chart_type from user input, sales dataframe and labels
+      chart=get_chart(chart_type, sales_df, labels=sales_df['date_created'].values)
+
+    #convert the dataframe to HTML
+      sales_df=sales_df.to_html()
+
+
+  #pack up data to be sent to template in the context dictionary
+  context={
+    'form': form,
+    'sales_df': sales_df,
+    'chart': chart
+    }
+
+  #load the sales/record.html page using the data that you just prepared
+  return render(request, 'sales/records.html', context)
 
 
